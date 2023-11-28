be0443ad72dd3f284c9ca84a67a26bbe035ca84d
KanaeMurakami <35243965+KanaeMurakami@users.noreply.github.com>
2023-11-28 22:29:24 +0900

    update トップページ実装

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,5 +2,17 @@
 
 # トップ
 class TopController < ApplicationController
-  def index; end
+  before_action :redirect_if_next_year
+
+  def index
+    @year = params[:year] || Time.current.next_year.year
+  end
+
+  private
+
+  def redirect_if_next_year
+    return if params[:year].to_i != Time.current.next_year.year
+
+    redirect_to root_url
+  end
 end
--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -2,11 +2,26 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Tops' do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/'
+RSpec.describe 'Top' do
+  describe 'GET #index' do
+    it '200が返却される' do
+      get root_path
       expect(response).to have_http_status(:success)
+    end
+
+    context ':year を指定した場合' do
+      it '200が返却される' do
+        get root_path(year: Time.current.year)
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context ':year で来年を指定した場合' do
+      it '302が返却される' do
+        get root_path(year: Time.current.next_year.year)
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(root_path)
+      end
     end
   end
 end
