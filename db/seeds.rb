d38b8e45467887e4e14ea62e0475f84737c0ce50
KanaeMurakami <35243965+KanaeMurakami@users.noreply.github.com>
2023-11-28 20:35:24 +0900

    update DBシード追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[
+  { display_order: 1, options: [{ text: '散歩', display_order: 1 }, { text: '読書', display_order: 2 }] },
+  { display_order: 2, options: [{ text: '晴れ', display_order: 1 }, { text: '雨', display_order: 2 }] },
+  { display_order: 3, options: [{ text: '甘いもの', display_order: 1 }, { text: '辛いもの', display_order: 2 }] },
+  { display_order: 4, options: [{ text: '海外旅行', display_order: 1 }, { text: '近場で旅行', display_order: 2 }] },
+  { display_order: 5, options: [{ text: '夜', display_order: 1 }, { text: '朝', display_order: 2 }] },
+  { display_order: 6, options: [{ text: '黄色', display_order: 1 }, { text: '水色', display_order: 2 }] },
+  { display_order: 7, options: [{ text: 'ランニング', display_order: 1 }, { text: '筋トレ', display_order: 2 }] },
+  { display_order: 8, options: [{ text: '冷たい色', display_order: 1 }, { text: '暖かい色', display_order: 2 }] }
+].each do |attribute|
+  question = Question.create!(display_order: attribute[:display_order])
+  attribute[:options].each do |e|
+    Option.create!(question:, text: e[:text], display_order: e[:display_order])
+  end
+end
